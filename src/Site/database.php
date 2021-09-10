4fd93aac06097a9a66b5c96a6e178248035fa04c
aveBHS <avebhs.ru@gmail.com>
2021-09-11 00:29:20 +0300

    Отслеживание ошибок подключения к БД и дебаг режим в конфиге

--- src/Site/database.php
+++ src/Site/database.php
@@ -1,9 +1,22 @@
 <?php
+
+use Site\Controllers\Exceptions\InternalServerErrorController;
+
 require_once $_SERVER['DOCUMENT_ROOT'] . "/../src/Site/helpers.php";
 
-return mysqli_connect(
+$link = mysqli_connect(
     env("DB_HOST"),
     env("DB_USER"),
     env("DB_PASSWD"),
     env("DB_BASE")
 );
+
+if(!$link){
+    if(env("debug")){
+        echo($link->error);
+    }
+    render(InternalServerErrorController::class, "view");
+    die();
+}
+
+return $link;
