ec74238db3ed15d73c5caa7336339785ed4e7307
aveBHS <avebhs.ru@gmail.com>
2021-09-10 23:34:07 +0300

    Файл конфигурации

--- src/Site/helpers.php
+++ src/Site/helpers.php
@@ -1,6 +1,27 @@
 <?php
 
-function render($className, $classMethod){
+function render($className, $classMethod)
+{
     $controller = new $className;
     echo $controller->$classMethod();
+}
+
+function env($paramName)
+{
+    $env = [];
+    $paramName = strtolower($paramName);
+    if ($file = fopen($_SERVER['DOCUMENT_ROOT'] . "/../.env", "r")) {
+        while(!feof($file)) {
+            $line = fgets($file);
+            if(empty(trim($line)) || substr(trim($line), 0, 2) == "//")
+                continue;
+            $param = explode("=", $line);
+            $env[strtolower(trim($param[0]))] = rtrim($param[1]);
+        }
+        fclose($file);
+    }
+    if(array_key_exists($paramName, $env)){
+        return $env[$paramName];
+    }
+    return Null;
 }
