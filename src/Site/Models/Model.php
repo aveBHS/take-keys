c0abce57e55c1de91682b7a8aac90f77e48341a2
aveBHS <avebhs.ru@gmail.com>
2021-09-11 00:30:07 +0300

    Абстрактная модель модели

---
+++ src/Site/Models/Model.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @var mysqli $db
+ */
+namespace Site\Models;
+
+abstract class Model
+{
+    private $db;
+
+    function __construct()
+    {
+        $this->db = require __DIR__ . "/../database.php";
+    }
+
+    function find($objectId)
+    {
+        $tableName = $this->tableName ?? getTableName(get_class($this));
+        $tableIndex = $this->tableId ?? 'id';
+
+        $query = $this->db->prepare(
+            "SELECT * FROM `$tableName` WHERE `$tableIndex`=? LIMIT 1"
+        );
+        if(!$query){
+            return Null;
+        }
+        $query->bind_param("s", $objectId);
+        if($query->execute()){
+            $result = $query->get_result();
+            if($result->num_rows < 1) return Null;
+            return (object) $result->fetch_assoc();
+        }
+        return Null;
+    }
+}
