e4e8e9d4d1d32caafc156d83daff78fffbb74f90
BHS <avebhs.ru@gmail.com>
2021-09-12 14:47:29 +0300

    Базовый шаблон карточки товара

--- src/Site/Controllers/Objects/CardController.php
+++ src/Site/Controllers/Objects/CardController.php
@@ -5,6 +5,7 @@ namespace Site\Controllers\Objects;
 use Site\Controllers\Controller;
 use Site\Controllers\Exceptions\NotFoundController;
 use Site\Core\HttpRequest;
+use Site\Models\ImageModel;
 use Site\Models\ObjectModel;
 
 class CardController implements Controller
@@ -13,7 +14,8 @@ class CardController implements Controller
         if(!$args[0]) $request->redirect("/");
         $object = (new ObjectModel())->find($args[0]);
         if(!is_null($object)){
-            view("objects.card", ["object" => $object]);
+            $images = (new ImageModel())->selectObjectImages($object->id);
+            $request->show(view("objects.card", ["object" => $object, "images" => $images]));
         } else {
             $request->returnException(new NotFoundController(), 404);
         }
