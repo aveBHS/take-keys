78ee8b3c2f8c293368203970a2f4ee477dba8524
aveBHS <avebhs.ru@gmail.com>
2021-09-21 10:18:54 +0300

    Модель запроса на автоподбор

---
+++ src/Site/Models/Request.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Site\Models;
+
+/**
+ * @property string phone
+ * @property string email
+ * @property string name
+ * @property float lat
+ * @property float lng
+ * @property int price
+ * @property string address
+ * @property int object_type
+ * @property int distance
+ */
+class Request extends Model
+{
+    protected $fields = [
+        'email', 'phone', 'messenger', 'object_type', 'price', 'distance', 'address',
+        'lat', 'lng', 'min_send_time', 'max_send_time'
+    ];
+}
