7a876c5e1cbce86119040154668942f8bc290d7a
aveBHS <avebhs.ru@gmail.com>
2021-09-29 19:03:17 +0300

    Поля модели объекта

--- src/Site/Models/ObjectModel.php
+++ src/Site/Models/ObjectModel.php
@@ -5,4 +5,10 @@ namespace Site\Models;
 class ObjectModel extends Model
 {
     protected $tableName = "objects";
+    protected $fields = [
+        "id", "title", "description", "address", "lat", "lng", "cost",
+        "name", "phones", "rooms", "floor", "floors", "sq",
+        "categoryId", "sectionId", "typeAd", "cityId", "regionId",
+        "metroSlug", "materialSlug", "isAd", "status"
+    ];
 }
