57b4dff30fe7b391b841767d1df75cfebabea84d
aveBHS <avebhs.ru@gmail.com>
2021-09-30 23:44:30 +0300

    Фикс бага посредника пользователя

--- src/Site/Middleware/UserMiddleware.php
+++ src/Site/Middleware/UserMiddleware.php
@@ -10,7 +10,7 @@ class UserMiddleware implements Middleware
     public function handle(HttpRequest $request, $callback, $args)
     {
         global $auth;
-        if($auth()){
+        if(!is_null($auth())){
             return call_user_func_array($callback, $args);
         }
         $request->setFlash("login_redirect", $request->getUrl());
