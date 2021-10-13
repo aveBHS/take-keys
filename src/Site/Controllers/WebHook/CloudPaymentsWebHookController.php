9eab5ac625bb1f1f03231d7c7a956dd8d60e42fb
aveBHS <avebhs.ru@gmail.com>
2021-10-14 01:26:32 +0300

    Установка метки о покупке по запросу от CloudPayments (не протестировано)

---
+++ src/Site/Controllers/WebHook/CloudPaymentsWebHookController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Site\Controllers\WebHook;
+
+use Site\Controllers\Exceptions\BadRequestController;
+use Site\Core\HttpRequest;
+use Site\Models\RequestModel;
+use Site\Models\UserModel;
+
+class CloudPaymentsWebHookController implements \Site\Controllers\Controller
+{
+
+    public function view(HttpRequest $request, $args)
+    {
+        $req = RequestModel::find($request->post("email"), "email");
+        if(!is_null($req)){
+            $req->purchased = 1;
+            try{
+                return $req->save();
+            } catch (\Exception $exception) {
+                $request->show($exception->getMessage());
+            }
+        } else {
+            $request->returnException(new BadRequestController(), 400);
+            return null;
+        }
+    }
+}
