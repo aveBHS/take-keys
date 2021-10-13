ba310ae80f38339d79aeeb90c97110574840e111
aveBHS <avebhs.ru@gmail.com>
2021-10-14 01:23:29 +0300

    Посредник CloudPayments (не протестировано)

---
+++ src/Site/Middleware/CloudPaymentsMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Site\Middleware;
+
+use Site\Controllers\Exceptions\ForbiddenController;
+use Site\Core\HttpRequest;
+
+class CloudPaymentsMiddleware implements Middleware
+{
+
+    public function handle(HttpRequest $request, $callback, $args)
+    {
+        $hash = hash_hmac('sha256', $request->body, env("cloudpayments_token"), true);
+        $hash = base64_encode($hash);
+        if($hash == $request->getHeader("X-Content-HMAC")){
+            return call_user_func_array($callback, $args);
+        } else {
+            $request->returnException(new ForbiddenController(), 403);
+        }
+        return null;
+    }
+}
