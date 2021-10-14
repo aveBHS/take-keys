5dc45a57fca20379ec04bcd6a4a75bd2f22cc1ea
aveBHS <avebhs.ru@gmail.com>
2021-10-14 03:00:14 +0300

    Оптимизация выборки изображений объектов

--- src/Site/Models/ImageModel.php
+++ src/Site/Models/ImageModel.php
@@ -6,13 +6,36 @@ use Site\Core\HttpRequest;
 
 class ImageModel extends \Site\Models\Model
 {
-    public static function selectObjectImages(int $objectId)
+    public static function selectObjectImagesById(array $objectsIds)
     {
-        $model = new self();
-        return $model->query(
-            "SELECT `path` FROM `images` WHERE `object_id` = ?",
-            [$objectId],
-            "i"
+        $objectsIdsString = [];
+        for($i = 0; $i < count($objectsIds); $i++)
+            if((int) $objectsIds[$i] > 0)
+                array_push($objectsIdsString, (int) $objectsIds[$i]);
+        $objectsIds = implode(",", $objectsIdsString);
+
+        return self::query(
+            "SELECT `path`, `object_id` FROM `images` WHERE `object_id` IN ($objectsIds)"
         );
+    }
+    public static function selectObjectImages(int $objectId){
+        return self::selectObjectImagesById([$objectId]);
+    }
+    public static function selectObjectsImages(array $objects): array
+    {
+        $objectsIds = [];
+        for($i = 0; $i < count($objects); $i++){
+            array_push($objectsIds, $objects[$i]->id);
+        }
+        $objectsImages = self::selectObjectImagesById($objectsIds);
+        for($i = 0; $i < count($objects); $i++){
+            $objects[$i]->images = [];
+            foreach ($objectsImages as $image){
+                if($image->object_id == $objects[$i]->id){
+                    array_push($objects[$i]->images, $image);
+                }
+            }
+        }
+        return $objects;
     }
 }
