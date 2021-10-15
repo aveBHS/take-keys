49cd1604062352ec459a9ebd40aa4f573d7c4b6d
aveBHS <avebhs.ru@gmail.com>
2021-10-15 16:36:57 +0300

    Запоминание токена авторизованного пользователя

--- src/Site/Core/AuthService.php
+++ src/Site/Core/AuthService.php
@@ -9,21 +9,23 @@ use Site\Models\UserModel;
 
 class AuthService
 {
+    protected $httpRequest;
     protected $model = null;
-    private $authMethod = null;
 
-    public function __construct($authMethod = UserModel::class)
+    public function __construct(HttpRequest $request)
     {
-        $this->authMethod = $authMethod;
+        $this->httpRequest = $request;
         if(isset($_SESSION['id'])){
-            $model = $this->authMethod::find($_SESSION['id']);
+            $model = UserModel::find($_SESSION['id']);
             if($model){
                 $this->model = $model;
                 $this->model->request = RequestModel::find($this->model->request_id);
             }
-        } else if($_COOKIE['auth_token']){
-            $model = $this->authMethod::find($_COOKIE['auth_token'], "token");
-            if($model) $this->authenticate($model);
+        } else if($this->httpRequest->getCookie("auth_token")){
+            $model = UserModel::find($this->httpRequest->getCookie("auth_token"), "remember_token");
+            if($model){
+                $this->authenticate($model);
+            }
         }
     }
 
@@ -32,15 +34,36 @@ class AuthService
         return $this->model;
     }
 
-    private function authenticate(Model $user)
+    public function genAuthToken(Model $user): string
     {
+        return md5($user->password."_".time()."_".$user->login);
+    }
+
+    private function authenticate(Model $user, bool $remember = true)
+    {
+        $this->model = $user;
         $_SESSION['id'] = $user->id;
+        if($remember) {
+            if(!empty($user->remember_token)){
+                $token_expires = time() + 60 * 60 * 24 * 30 * 12;
+                $this->httpRequest->setCookie('auth_token', $user->remember_token, false, $token_expires);
+            } else {
+                $user->remember_token = $this->genAuthToken($user);
+                try {
+                    $user->save();
+                    $token_expires = time() + 60 * 60 * 24 * 30 * 12;
+                    $this->httpRequest->setCookie('auth_token', $user->remember_token, false, $token_expires);
+                } catch (\Exception $exception) {
+                    die($exception->getMessage());
+                }
+            }
+        }
     }
 
     public function login(string $login, string $password): bool
     {
         if($this->model) return true;
-        $user = $this->authMethod::find(strtolower($login), "login");
+        $user = UserModel::find(strtolower($login), "login");
         if($user){
             if($user->password == md5($password)){
                 $this->authenticate($user);
@@ -55,11 +78,15 @@ class AuthService
     public function logout()
     {
         session_destroy();
+        session_start();
     }
 
-    public function tempLogin(int $id)
+    public function directLogin(int $request_id, bool $by_request_id = true)
     {
-        $request = UserModel::find($id, "request_id");
+        if($by_request_id)
+            $request = UserModel::find($request_id, "request_id");
+        else
+            $request = UserModel::find($request_id);
         if (!is_null($request)) $this->authenticate($request);
     }
 }
