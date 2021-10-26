14621d86514b74a58e738e37adb122c232a5bd30
aveBHS <avebhs.ru@gmail.com>
2021-10-26 23:25:12 +0300

    Отправка уведомления в телеграм через сервис

--- src/Site/Controllers/WebHook/PactNotifyController.php
+++ src/Site/Controllers/WebHook/PactNotifyController.php
@@ -3,14 +3,14 @@
 namespace Site\Controllers\WebHook;
 
 use Site\Core\HttpRequest;
+use Site\Core\TelegramNotifyService;
 
 class PactNotifyController implements \Site\Controllers\Controller
 {
 
     public function view(HttpRequest $request, $args)
     {
-        $token = env('TELEGRAM_KEY');
-        $chat_id = env('TELEGRAM_CHAT');
+        $tg = new TelegramNotifyService(env('TELEGRAM_KEY'));
         $message = file_get_contents("php://input");
         if(json_decode($message, true)['type'] == "system") {
             $json_beautified = str_replace(
@@ -18,13 +18,7 @@ class PactNotifyController implements \Site\Controllers\Controller
                 array("{\n    ", "\n}", "\",\n    \""),
                 $message
             );
-            $message = urlencode($json_beautified);
-            $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message";
-
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_exec($ch);
-            curl_close($ch);
+            $tg->send(env('TELEGRAM_CHAT'), $json_beautified);
         }
     }
 }
