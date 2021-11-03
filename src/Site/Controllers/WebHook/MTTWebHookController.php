03d9cb6900ef458d9eafcc078feb526a986d9a49
aveBHS <avebhs.ru@gmail.com>
2021-11-03 22:31:49 +0300

    Получение результата звонка от МТТ

--- src/Site/Controllers/WebHook/MTTWebHookController.php
+++ src/Site/Controllers/WebHook/MTTWebHookController.php
@@ -3,14 +3,25 @@
 namespace Site\Controllers\WebHook;
 
 use Site\Core\HttpRequest;
+use Site\Models\PhoneCallModel;
 
 class MTTWebHookController implements \Site\Controllers\Controller
 {
 
     public function view(HttpRequest $request, $args)
     {
-        file_put_contents("mtt_post.txt", var_export($request->post(), true));
-        file_put_contents("mtt_get.txt", var_export($request->get(), true));
-        file_put_contents("mtt_json.txt", var_export($request->json(), true));
+        $time = time();
+        file_put_contents("./logs/mtt/mtt_json_$time.txt", var_export($request->json(), true));
+
+        if($request->json("event") == "h"){
+            $call = PhoneCallModel::find($request->json("callId"), "call_id");
+            if(!is_null($call)){
+                $call->call_status = $request->json("status") == "active" ?
+                    PhoneCallModel::callStatuses["SUCCESS"] : PhoneCallModel::callStatuses["FAIL"];
+                try {
+                    $call->save();
+                } catch (\Exception $e) {}
+            }
+        }
     }
 }
--- src/Site/Models/PhoneCallModel.php
+++ src/Site/Models/PhoneCallModel.php
@@ -5,9 +5,13 @@ namespace Site\Models;
 class PhoneCallModel extends Model
 {
     protected $tableName = "calls";
-    protected $fields = ['phone', 'call_type'];
+    protected $fields = ['phone', 'call_type', 'call_status'];
     const callTypes = [
         "REGISTRATION" => 0,
-        "OWNER_CHECK" => 1
+        "OWNER_CHECK"  => 1,
+        "ADS_OBJECT"   => 2
+    ];
+    const callStatuses = [
+        "NEW", "CREATED", "SUCCESS", "FAIL"
     ];
 }
