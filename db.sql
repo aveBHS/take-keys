4a4aec8e1ae5a56e8902fd5b303cfae8f9d070bb
aveBHS <avebhs.ru@gmail.com>
2021-11-12 15:42:30 +0300

    Дамп БД

--- db.sql
+++ db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5ubuntu0.5
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Хост: localhost:3306
--- Время создания: Окт 29 2021 г., 19:01
--- Версия сервера: 5.7.36-0ubuntu0.18.04.1
--- Версия PHP: 7.2.24-0ubuntu0.18.04.10
+-- Хост: 127.0.0.1:3306
+-- Время создания: Ноя 12 2021 г., 15:19
+-- Версия сервера: 8.0.19
+-- Версия PHP: 7.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `property`
+-- База данных: `inpars`
 --
 
 -- --------------------------------------------------------
@@ -27,14 +28,30 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `calls` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `call_id` varchar(25) DEFAULT NULL,
   `phone` varchar(15) NOT NULL,
-  `call_status` int(11) NOT NULL DEFAULT '0' COMMENT '0 - Новая заявка\r\n1 - Заявка отправлена\r\n2 - Получен результат',
-  `call_type` int(11) NOT NULL DEFAULT '0' COMMENT '0 - Звонок после регистрации\r\n1 - Звонок собственнику по запросу',
-  `result_id` int(11) DEFAULT NULL,
+  `call_status` int NOT NULL DEFAULT '0' COMMENT '0 - Новая заявка\r\n1 - Заявка отправлена\r\n2 - Получен результат\r\n3 - Звонок не удался\r\n4 - Попытки закончились',
+  `call_type` int NOT NULL DEFAULT '0' COMMENT '0 - Звонок после регистрации\r\n1 - Звонок собственнику по запросу\r\n2 - Звонок с рекламного объекта',
+  `attempts` int NOT NULL DEFAULT '0',
+  `next_attempt` int DEFAULT NULL,
+  `result_id` int DEFAULT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `calls_ads`
+--
+
+CREATE TABLE `calls_ads` (
+  `id` int NOT NULL,
+  `phone_from` varchar(12) NOT NULL,
+  `phone_to` varchar(12) NOT NULL,
+  `result` int NOT NULL DEFAULT '0' COMMENT '0 - нет информации\r\n1 - агент\r\n2 - клиент',
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -43,12 +60,12 @@ CREATE TABLE `calls` (
 --
 
 CREATE TABLE `call_results` (
-  `id` int(11) NOT NULL,
-  `is_owner` int(11) NOT NULL,
-  `is_actual` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `is_owner` int NOT NULL,
+  `is_actual` int NOT NULL,
   `record` text NOT NULL,
   `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -57,11 +74,10 @@ CREATE TABLE `call_results` (
 --
 
 CREATE TABLE `images` (
-  `id` int(11) NOT NULL,
-  `object_id` int(11) NOT NULL,
-  `path` text NOT NULL,
-  `isAd` int(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int NOT NULL,
+  `object_id` int NOT NULL,
+  `path` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -70,67 +86,10 @@ CREATE TABLE `images` (
 --
 
 CREATE TABLE `megafon_links` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `number` text NOT NULL,
   `link` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `megafon_links`
---
-
-INSERT INTO `megafon_links` (`id`, `number`, `link`) VALUES
-(2, '79388715974', 'https://take-keys.com/id17297526'),
-(3, '79388734695', 'https://take-keys.com/id167354685'),
-(4, '79384530942', 'https://take-keys.com/id187535689'),
-(5, '79384719579', 'https://take-keys.com/id137284654'),
-(6, '79384677562', 'https://take-keys.com/id17378004'),
-(7, '79384692672', 'https://take-keys.com/id17297526'),
-(8, '79384933301', 'https://take-keys.com/id163735736'),
-(9, '79384911233', 'https://take-keys.com/id17864728'),
-(10, '79384678883', 'https://take-keys.com/id1834528235'),
-(11, '79384919455', 'https://take-keys.com/id176369369'),
-(12, '79388739174', 'https://take-keys.com/id14268358'),
-(13, '79384527173', 'https://take-keys.com/id18649368'),
-(14, '79384605327', 'https://take-keys.com/id13285367'),
-(15, '79384521496', 'https://take-keys.com/id15473273'),
-(16, '79384637106', 'https://take-keys.com/id163157312'),
-(17, '79384916690', 'https://take-keys.com/id14562357'),
-(18, '79384882515', 'https://take-keys.com/id165367908'),
-(20, '79384547046', 'https://take-keys.com/id14372635'),
-(21, '79384909738', 'https://take-keys.com/id13468345'),
-(22, '79388732644', 'https://take-keys.com/id1546723477'),
-(23, '79388727549', 'https://take-keys.com/id1674374389'),
-(24, '79384606858', 'https://take-keys.com/id19345728'),
-(25, '79384540725', 'https://take-keys.com/id16734573'),
-(26, '79384518676', 'https://take-keys.com/id167278245'),
-(27, '79384967646', 'https://take-keys.com/id173426724'),
-(28, '79384521081', 'https://take-keys.com/id146724778'),
-(29, '79384898057', 'https://take-keys.com/id17346836'),
-(30, '79384662119', 'https://take-keys.com/id14257853'),
-(31, '79384890025', 'https://take-keys.com/id167345832'),
-(32, '79384944936', 'https://take-keys.com/id1529743'),
-(33, '79384521959', 'https://take-keys.com/id1637838'),
-(34, '79384923221', 'https://take-keys.com/id1393678'),
-(35, '79384934871', 'https://take-keys.com/id178463894'),
-(36, '79384910894', 'https://take-keys.com/id15463467'),
-(37, '79388723352', 'https://take-keys.com/id11219826'),
-(38, '79384567668', 'https://take-keys.com/id17563724'),
-(39, '79384569007', 'https://take-keys.com/id1326842'),
-(40, '79384669343', 'https://take-keys.com/id135637345'),
-(41, '79388716235', 'https://take-keys.com/id124356784'),
-(42, '79384974350', 'https://take-keys.com/id14245785'),
-(43, '79388726155', 'https://take-keys.com/id13256346'),
-(44, '79384537116', 'https://take-keys.com/id167437936'),
-(45, '79384954737', 'https://take-keys.com/id12345723'),
-(46, '79384510264', 'https://take-keys.com/id1387337'),
-(47, '79388754536', 'https://take-keys.com/id1352365'),
-(48, '79388700225', 'https://take-keys.com/id1463782'),
-(49, '79388780234', 'https://take-keys.com/id13256725'),
-(50, '79384919066', 'https://take-keys.com/id123452764'),
-(51, '79384633137', 'https://take-keys.com/id145623723'),
-(52, '79384898740', 'https://take-keys.com/id12526634'),
-(53, '79388721563', 'https://take-keys.com/id14562677');
 
 -- --------------------------------------------------------
 
@@ -139,13 +98,14 @@ INSERT INTO `megafon_links` (`id`, `number`, `link`) VALUES
 --
 
 CREATE TABLE `notifies` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `address` text,
   `text` text NOT NULL,
   `type` varchar(255) NOT NULL,
-  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0 - новый\r\n1 - принят\r\n2 - обработан',
+  `status` int NOT NULL DEFAULT '0' COMMENT '0 - новый\r\n1 - принят\r\n2 - обработан',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -154,32 +114,33 @@ CREATE TABLE `notifies` (
 --
 
 CREATE TABLE `objects` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL,
+  `inpars_id` bigint DEFAULT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `lat` float NOT NULL,
   `lng` float NOT NULL,
   `address` text NOT NULL,
-  `cost` int(11) NOT NULL,
-  `metroId` int(11) NOT NULL,
-  `name` text,
+  `cost` int NOT NULL,
+  `metroId` int DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
   `phones` text NOT NULL,
-  `rooms` int(11) NOT NULL,
-  `floor` int(11) NOT NULL,
-  `floors` int(11) NOT NULL,
+  `rooms` int NOT NULL,
+  `floor` int NOT NULL,
+  `floors` int NOT NULL,
   `sq` float NOT NULL,
-  `categoryId` int(11) NOT NULL,
-  `sectionId` int(11) NOT NULL,
-  `typeAd` int(11) NOT NULL,
-  `cityId` int(11) NOT NULL,
-  `regionId` int(11) NOT NULL,
+  `categoryId` int NOT NULL,
+  `sectionId` int NOT NULL,
+  `typeAd` int NOT NULL,
+  `cityId` int NOT NULL,
+  `regionId` int NOT NULL,
   `metroSlug` text,
   `materialSlug` text,
-  `source` varchar(100) NOT NULL,
-  `isAd` int(11) NOT NULL DEFAULT '0' COMMENT '0 - нет, 1 - да',
-  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0 - активен\r\n1 - проверка\r\n2 - архив',
+  `source` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `isAd` int NOT NULL DEFAULT '0' COMMENT '0 - нет, 1 - да',
+  `status` int NOT NULL DEFAULT '0' COMMENT '0 - активен\r\n1 - проверка\r\n2 - архив',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -188,12 +149,12 @@ CREATE TABLE `objects` (
 --
 
 CREATE TABLE `object_types` (
-  `object_type_id` int(11) NOT NULL,
-  `object_type_slug` text NOT NULL,
-  `price_adder` int(11) NOT NULL,
-  `price_subtractor` int(11) NOT NULL,
-  `inpars_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `object_type_id` int NOT NULL,
+  `object_type_slug` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `price_adder` int NOT NULL,
+  `price_subtractor` int NOT NULL,
+  `inpars_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `object_types`
@@ -207,31 +168,49 @@ INSERT INTO `object_types` (`object_type_id`, `object_type_slug`, `price_adder`,
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `amount` int NOT NULL,
+  `token` text,
+  `attempts` int NOT NULL DEFAULT '0',
+  `next_attempt` int DEFAULT NULL,
+  `status` int NOT NULL DEFAULT '0' COMMENT '0 - Не оплачена регистрация\r\n1 - Ожидание времени оплаты\r\n2 - Оплачено\r\n3 - Попытки закончились',
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `requests`
 --
 
 CREATE TABLE `requests` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `messenger` text,
-  `object_type` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `distance` int(11) NOT NULL DEFAULT '1000',
+  `id` int NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `messenger` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `object_type` int NOT NULL,
+  `price` int NOT NULL,
+  `distance` int NOT NULL DEFAULT '1000',
   `address` varchar(255) DEFAULT NULL,
   `lat` float NOT NULL DEFAULT '0',
   `lng` float NOT NULL DEFAULT '0',
-  `min_send_time` varchar(5) NOT NULL DEFAULT '8:00',
-  `max_send_time` varchar(5) NOT NULL DEFAULT '22:00',
-  `last_result` text,
+  `min_send_time` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '8:00',
+  `max_send_time` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '22:00',
+  `last_result` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `recommendations` text,
-  `whatsapp_sent` int(11) NOT NULL DEFAULT '0',
-  `purchased` int(11) NOT NULL DEFAULT '0',
-  `is_free` int(11) NOT NULL DEFAULT '1',
-  `status` int(11) NOT NULL DEFAULT '1' COMMENT '0 - Деактивация\r\n1 - Новая заявка\r\n2 - Идет отправка промо\r\n3 - Ожидание брони\r\n4 - Готов к отправке подбора\r\n5 - Идет отправка подбора',
+  `favorites` text,
+  `whatsapp_sent` int NOT NULL DEFAULT '0',
+  `purchased` int NOT NULL DEFAULT '0',
+  `is_free` int NOT NULL DEFAULT '1',
+  `status` int NOT NULL DEFAULT '1' COMMENT '0 - Деактивация\r\n1 - Новая заявка\r\n2 - Идет отправка промо\r\n3 - Ожидание брони\r\n4 - Готов к отправке подбора\r\n5 - Идет отправка подбора',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -240,12 +219,12 @@ CREATE TABLE `requests` (
 --
 
 CREATE TABLE `reviews` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(255) NOT NULL,
   `ava` varchar(255) NOT NULL,
   `text` varchar(1000) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `reviews`
@@ -265,17 +244,17 @@ INSERT INTO `reviews` (`id`, `name`, `ava`, `text`, `created`) VALUES
 --
 
 CREATE TABLE `updates` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `type` text NOT NULL,
-  `time` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `time` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `updates`
 --
 
 INSERT INTO `updates` (`id`, `type`, `time`) VALUES
-(1, 'parser', 1635523251);
+(1, 'parser', 500051);
 
 -- --------------------------------------------------------
 
@@ -284,17 +263,17 @@ INSERT INTO `updates` (`id`, `type`, `time`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `login` varchar(255) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `name` varchar(255) NOT NULL DEFAULT 'Пользователь',
+  `name` varchar(255) NOT NULL,
   `remember_token` varchar(128) DEFAULT NULL,
   `email_token` varchar(32) DEFAULT NULL,
   `reset_token` varchar(128) DEFAULT NULL,
-  `request_id` int(11) NOT NULL DEFAULT '-1',
-  `admin` int(11) NOT NULL DEFAULT '0',
+  `request_id` int NOT NULL DEFAULT '-1',
+  `admin` int NOT NULL DEFAULT '0',
   `joined` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Индексы сохранённых таблиц
@@ -304,6 +283,12 @@ CREATE TABLE `users` (
 -- Индексы таблицы `calls`
 --
 ALTER TABLE `calls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `calls_ads`
+--
+ALTER TABLE `calls_ads`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -344,6 +329,13 @@ ALTER TABLE `object_types`
   ADD PRIMARY KEY (`object_type_id`);
 
 --
+-- Индексы таблицы `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
 -- Индексы таблицы `requests`
 --
 ALTER TABLE `requests`
@@ -377,52 +369,81 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `calls`
 --
 ALTER TABLE `calls`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `calls_ads`
+--
+ALTER TABLE `calls_ads`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `call_results`
 --
 ALTER TABLE `call_results`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145969;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `megafon_links`
 --
 ALTER TABLE `megafon_links`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `notifies`
 --
 ALTER TABLE `notifies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `objects`
+--
+ALTER TABLE `objects`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `object_types`
 --
 ALTER TABLE `object_types`
-  MODIFY `object_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `object_type_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=286;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT для таблицы `updates`
 --
 ALTER TABLE `updates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=279;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
