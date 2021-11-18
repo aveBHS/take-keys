44ef2670ffd3867069a2b639a1a55a3935ed280d
aveBHS <avebhs.ru@gmail.com>
2021-11-18 16:01:20 +0300

    Удаление комментариев из платежного виджета

--- src/Site/Views/layout/payment_widget.php
+++ src/Site/Views/layout/payment_widget.php
@@ -10,20 +10,17 @@ $amount = $amount ?? env("cloudpayments_subscribe_amount");
 <script>
 this.pay = function (user_id) {
     var widget = new cp.CloudPayments();
-    widget.pay('charge', // или 'charge'
-        { //options
-            publicId: '<?=env("cloudpayments_public_key")?>', //id из личного кабинета
-            description: '<?=env("cloudpayments_subscribe_desc")?>', //назначение
-            amount: <?=$amount?>, //сумма
-            currency: 'RUB', //валюта
-            accountId: user_id, //идентификатор плательщика (необязательно)
-            skin: "mini", //дизайн виджета (необязательно)
-            data: {
-                myProp: 'myProp value'
-            }
+    widget.pay('charge',
+        {
+            publicId: '<?=env("cloudpayments_public_key")?>',
+            description: '<?=env("cloudpayments_subscribe_desc")?>',
+            amount: <?=$amount?>,
+            currency: 'RUB',
+            accountId: user_id,
+            skin: "mini"
         },
         {
-            onSuccess: function (options) { // success
+            onSuccess: function (options) {
                 $("#mobile_action_block").html(`
                     <button type="button" class="btn btn-48 btn-primary" onclick="send_call_request();">Связаться</button>
                     <a href="https://take-keys.com/booking"><button class="btn btn-48 btn-dark">Бронировать</button></a>`)
@@ -32,7 +29,7 @@ this.pay = function (user_id) {
                     <a href="https://take-keys.com/booking"><button class="btn btn-48 btn-dark w-100 mb-4">Бронировать</button></a>`)
                 Modal.getOrCreateInstance($("#popup-tarif-pay-success")).show()
             },
-            onFail: function (reason, options) { // fail
+            onFail: function (reason, options) {
                 Modal.getOrCreateInstance($("#popup-tarif-pay-fail")).show()
             }
         }
