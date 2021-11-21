f5a4d7f49536b9902b42bd4fe75dcbd3f7c758d5
aveBHS <avebhs.ru@gmail.com>
2021-11-21 19:31:22 +0300

    Обработка рекуррентных платежей

--- src/Site/Controllers/WebHook/CloudPaymentsWebHookController.php
+++ src/Site/Controllers/WebHook/CloudPaymentsWebHookController.php
@@ -20,6 +20,17 @@ class CloudPaymentsWebHookController implements \Site\Controllers\Controller
             $request->setHeader("Content-Type", "application/json");
             $request->show('{"code":0}');
 
+            $data = $request->post("Data");
+            if(!is_null($data)){
+                $data = json_decode($data);
+                if(!is_null($data)){
+                    if($data->type == "recurrent"){
+                        $tg->send(env("TELEGRAM_USERS_ACTIONS_CHAT"), "Успешная рекуррентная оплата на сумму {$request->post('PaymentAmount')} RUB, ID{$request->post('AccountId')}");
+                        return;
+                    }
+                }
+            }
+
             $amount = (int)$request->post('PaymentAmount');
             if ($amount != (int)env("first_payment_amount_sale") && $amount != (int)env("first_payment_amount") || $request->post("Status") != "Completed") {
                 $tg->send(env("TELEGRAM_USERS_ACTIONS_CHAT"), "Оплата на подмененную сумму - {$request->post('PaymentAmount')} руб, платеж отклонен, ID{$request->post('AccountId')}\nТокен: {$request->post('Token')}");
