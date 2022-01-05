0656916f729128bd00501f36f7ee5e1541a157a4
aveBHS <avebhs.ru@gmail.com>
2022-01-06 00:21:46 +0300

    Обновление выборки изображений

--- src/Site/Models/ImageModel.php
+++ src/Site/Models/ImageModel.php
@@ -9,20 +9,8 @@ class ImageModel extends \Site\Models\Model
     protected $fields = [
         "object_id", "path"
     ];
-    public static function selectObjectImagesById(array $objectsIds)
-    {
-        $objectsIdsString = [];
-        for($i = 0; $i < count($objectsIds); $i++)
-            if((int) $objectsIds[$i] > 0)
-                array_push($objectsIdsString, (int) $objectsIds[$i]);
-        $objectsIds = implode(",", $objectsIdsString);
-
-        return self::query(
-            "SELECT `path`, `object_id` FROM `images` WHERE `object_id` IN ($objectsIds)"
-        );
-    }
     public static function selectObjectImages(int $objectId){
-        return self::selectObjectImagesById([$objectId]);
+        return self::select([['object_id', [[$objectId], 'in']]]);
     }
     public static function selectObjectsImages(array $objects): array
     {
@@ -30,7 +18,7 @@ class ImageModel extends \Site\Models\Model
         for($i = 0; $i < count($objects); $i++){
             array_push($objectsIds, $objects[$i]->id);
         }
-        $objectsImages = self::selectObjectImagesById($objectsIds);
+        $objectsImages = self::select([['object_id', [$objectsIds, 'in']]]);
         for($i = 0; $i < count($objects); $i++){
             $objects[$i]->images = [];
             foreach ($objectsImages as $image){
