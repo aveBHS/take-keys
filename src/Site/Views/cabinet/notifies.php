d115a1c5b137a520d4389ff1108213fbe94055a0
aveBHS <avebhs.ru@gmail.com>
2022-02-07 11:02:12 +0300

    Удалил лишнее уведомление

--- src/Site/Views/cabinet/notifies.php
+++ src/Site/Views/cabinet/notifies.php
@@ -12,7 +12,6 @@ global $auth;
 <?=view("layout.lk.templ_open", ["current_page" => LK_NOTIFIES_PAGE])?>
 <h1 class="h1 item__title h-64 my-4">Уведомления</h1>
 <div class="lk-notifications">
-    <?=view("layout.lk.notification_vip")?>
     <?php foreach($notifies as $notify) {
         switch ($notify->type){
             case "call":
